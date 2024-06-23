f72fe52663cce28eeb7e9db781714ce176f76402
SaifulHD <ihsansaifulhadi7@gmail.com>
2024-06-23 22:15:01 +0700

    make a blog page and single page postt

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>
+        {{ $title }}
+    </x-slot:title>
+    <h3 class="text-xl">
+        Halaman Home
+    </h3>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,89 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Ihsan Saiful Hadi', 'title' => 'About Page']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' =>
+    [
+        [
+            'id' => '1',
+            'slug' => 'judul-article-1',
+            'title' => 'Judul article 1',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-article-2',
+            'title' => 'Judul article 2',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ],
+        [
+            'id' => '3',
+            'slug' => 'judul-article-3',
+            'title' => 'Judul article 3',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ]
+
+    ]]);
+});
+
+Route::get('/post/{slug}', function ($slug) {
+    $post = [
+        [
+            'id' => '1',
+            'slug' => 'judul-article-1',
+            'title' => 'Judul article 1',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-article-2',
+            'title' => 'Judul article 2',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ],
+        [
+            'id' => '3',
+            'slug' => 'judul-article-3',
+            'title' => 'Judul article 3',
+            'author' => 'Ihsan Saiful Hadi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab perspiciatis nobis maiores reprehenderit illo
+            nihil inventore aliquid voluptatibus unde, vel quod dignissimos voluptas ullam veritatis aperiam distinctio
+            dolorem minus tempora.'
+        ]
+
+    ];
+
+    $post = Arr::first($post, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Page']);
 });
